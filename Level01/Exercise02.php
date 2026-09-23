8d7708bfbf573eee9344c5b5e57fed7e9b128625
Tamara <tamara_tmb@hotmail.com>
2026-09-23 11:31:18 +0200

    fix: personal comments

--- Level01/Exercise02.php
+++ Level01/Exercise02.php
@@ -6,10 +6,15 @@
 // Triángulo y Rectángulo que hereden de Shape y que calculen 
 // respectivamente el área con un método.
 
+
+// Declaración de la clase base Shape
+// (pt) Shape é apenas uma classe base que representa uma forma geométrica com largura e altura.
+
 class Shape {
-    public $width;
+    public $width;  // (pt) public porque pode ser acessada diretamente pelas subclasses
     public $height;
 
+    // Constructor
     public function __construct($width, $height) {
         $this->width = $width;
         $this->height = $height;
@@ -22,13 +27,13 @@ class Triangle extends Shape {
     }
 }
 
-class Rectangle extends Shape {
+class Rectangle extends Shape { // (pt) Rectangle é uma subclasse de Shape que representa um retângulo.
     public function area() {
         return $this->width * $this->height;
     }
 }
 
-$triangle = new Triangle(10, 5);
+$triangle = new Triangle(10, 5); // (pt) Criando uma instância da classe Triangle com largura 10 e altura 5
 echo "Área del triángulo: " . $triangle->area() . PHP_EOL;
 
 $rectangle = new Rectangle(10, 5);
